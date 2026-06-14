5bb9605fd33fa8eac99bcde617c83d6687d3aec8
Ankit Vishwakarma <ankvish1102@gmail.com>
2026-06-14 16:37:42 +0530

    added project

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 import CustomCursor from './utils/CursorAnimation'
 import Certifications from './components/Certifications'
-import Experience from './components/Experience'
+// import Experience from './components/Experience'
 
 export default function App() {
   return (
@@ -18,7 +18,7 @@ export default function App() {
       <Home />
       <Skills />
       <Projects />
-      <Experience />
+      {/* <Experience /> */}
       <Certifications />
       <About />
       <Contact />
