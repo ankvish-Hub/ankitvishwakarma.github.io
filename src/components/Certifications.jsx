9d24039a99a8670b5d98cd2d41a1f1b35ab72ea1
Ankit Vishwakarma <ankvish1102@gmail.com>
2026-06-25 22:43:07 +0530

    Added Namaste JavaScript certification to portfolio

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -1,14 +1,50 @@
-
 import { motion } from "framer-motion";
 
 export default function Certifications() {
   const certifications = [
-    { id: 1, title: "AWS - Solutions Architecture Job Simulation", issuer: "Forage", link: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/pmnMSL4QiQ9JCgE3W/kkE9HyeNcw6rwCRGw_pmnMSL4QiQ9JCgE3W_zz3mDT8SJjFMCeL7b_1751981716204_completion_certificate.pdf", date: "July 2025" },
-    { id: 2, title: "DataCom - Software Development", issuer: "Forage", link: "https://www.theforage.com/completion-certificates/gCW7Xki5Y3vNpBmnn/L3NcyCoAjLno9d3T9_gCW7Xki5Y3vNpBmnn_zz3mDT8SJjFMCeL7b_1753363895210_completion_certificate.pdf", date: "July 2025" },
-    { id: 3, title: "SQL (Basic) Certificate", issuer: "HackerRank", link: "https://www.hackerrank.com/certificates/iframe/56369e0ca5f3", date: "Ju1y 2025" },
-    { id: 4, title: "Accenture Nordics - Software Engineering Job Simulation", issuer: "Forage", link: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/xhih9yFWsf6AYfngd/HNpZwZcuYwona2d8Y_xhih9yFWsf6AYfngd_zz3mDT8SJjFMCeL7b_1751551067691_completion_certificate.pdf", date: "June 2025" },
-    { id: 5, title: "Deloitte Australia - Technology  Job Simulation", issuer: "Forage", link: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/9PBTqmSxAf6zZTseP/udmxiyHeqYQLkTPvf_9PBTqmSxAf6zZTseP_zz3mDT8SJjFMCeL7b_1751462292744_completion_certificate.pdf", date: "June 2025" },
-  
+    {
+      id: 1,
+      title: "Namaste JavaScript",
+      issuer: "NamasteDev",
+      link: "/assets/certificate.webp", 
+      date: "June 2026",
+    },
+
+    {
+      id: 2,
+      title: "AWS - Solutions Architecture Job Simulation",
+      issuer: "Forage",
+      link: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/pmnMSL4QiQ9JCgE3W/kkE9HyeNcw6rwCRGw_pmnMSL4QiQ9JCgE3W_zz3mDT8SJjFMCeL7b_1751981716204_completion_certificate.pdf",
+      date: "July 2025",
+    },
+    {
+      id: 3,
+      title: "DataCom - Software Development",
+      issuer: "Forage",
+      link: "https://www.theforage.com/completion-certificates/gCW7Xki5Y3vNpBmnn/L3NcyCoAjLno9d3T9_gCW7Xki5Y3vNpBmnn_zz3mDT8SJjFMCeL7b_1753363895210_completion_certificate.pdf",
+      date: "July 2025",
+    },
+    {
+      id: 4,
+      title: "SQL (Basic) Certificate",
+      issuer: "HackerRank",
+      link: "https://www.hackerrank.com/certificates/iframe/56369e0ca5f3",
+      date: "Ju1y 2025",
+    },
+    {
+      id: 5,
+      title: "Accenture Nordics - Software Engineering Job Simulation",
+      issuer: "Forage",
+      link: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/xhih9yFWsf6AYfngd/HNpZwZcuYwona2d8Y_xhih9yFWsf6AYfngd_zz3mDT8SJjFMCeL7b_1751551067691_completion_certificate.pdf",
+      date: "June 2025",
+    },
+    {
+      id: 6,
+      title: "Deloitte Australia - Technology  Job Simulation",
+      issuer: "Forage",
+      link: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/9PBTqmSxAf6zZTseP/udmxiyHeqYQLkTPvf_9PBTqmSxAf6zZTseP_zz3mDT8SJjFMCeL7b_1751462292744_completion_certificate.pdf",
+      date: "June 2025",
+    },
   ];
   return (
     <div className="w-full my-8 py-8 lg:my-16 lg:py-16" id="certifications">
@@ -22,7 +58,7 @@ export default function Certifications() {
         License & <span className="font-extrabold">Certifications</span>
       </motion.h2>
       <div className="px-5 lg:px-28 my-8 lg:mt-16 space-y-6">
-        {certifications.map(cert => (
+        {certifications.map((cert) => (
           <motion.a
             key={cert.id}
             href={cert.link}
@@ -39,11 +75,13 @@ export default function Certifications() {
                 <div className="font-bold text-lg">{cert.title}</div>
                 <div className="text-sm text-gray-500">{cert.issuer}</div>
               </div>
-              <div className="text-xs text-gray-400 lg:text-right">{cert.date}</div>
+              <div className="text-xs text-gray-400 lg:text-right">
+                {cert.date}
+              </div>
             </div>
           </motion.a>
         ))}
       </div>
     </div>
   );
-} 
+}
